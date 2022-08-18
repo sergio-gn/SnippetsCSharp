aa509f08ca1240d337204d86c64f4b9ceaf64bd1
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2022-08-18 01:47:06 -0300

    Create PassVariableAsParameter.cs

---
+++ PassVariableAsParameter.cs
@@ -0,0 +1,14 @@
+    public int health = 100;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
